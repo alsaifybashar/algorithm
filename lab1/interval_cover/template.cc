469206d2428bdba885cf6b61428b3a26a0776b2a
Bashar Al-Saify <bashar.alsaify@yahoo.com>
2026-02-02 14:36:18 +0000

    Adding documentation for lab1

---
+++ lab1/interval_cover/template.cc
@@ -0,0 +1,30 @@
+// Author: Bashar Al-Saify
+
+// Algorithm
+// -----------------------------------------------
+//
+// ------------------------------------------------
+
+// Complexity
+//
+
+// Command to run the program
+// ----------------------------------
+// g++ main.cc < example_file.txt
+
+#include <map>
+#include <string>
+
+using namespace std;
+
+string line;
+string key;
+string value;
+map<string, string> dictionary;
+
+int main() {
+
+  // code here
+
+  return 0;
+}
